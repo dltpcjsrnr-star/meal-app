58d879cfc5b662287760fb86e75ff3b8b845275d
dltpcjsrnr-star <dltpcjsrnr@gmail.com>
2026-06-21 11:40:06 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE = 'meal-app-v1';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE)));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
